e6b4dc7e80b909891588dd4228a0d8b6af930d19
Andrey <andreyshanorm@MacBook-Air-Andrey.local>
2024-03-26 23:59:31 +0400

    after first review

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,19 @@
-export type ProductStatus = 'софт-скил' | 'хард-скил' | 'кнопка' | 'другое' | 'дополнительное';
+type ProductStatus = 'софт-скил' | 'хард-скил' | 'кнопка' | 'другое' | 'дополнительное';
+
+export enum Events {
+  CATALOG_PRODUCTS = "product:changed", //все категории карточек
+  HOVER_PRODUCTS = "product:hover",//наведение на карточку
+  CLICK_PRODUCTS = "card:open", //клик по карточке
+  OPEN_MODAL = "modal:open", //при клике на карточку открывается модальное окно
+  CLOSE_MODAL = "modal:close",//приклике на крест закрывется модальное окно
+  CHANGING_PRODUCT_CART = "cart:changed",//добавить продукт в корзину
+  OPEN_CART = "cart:open",//открытие корзины
+  DELETE_PRODUCT ="prodact:remove",//удалить продукт из корзины
+  MAKING_AN_ORDER = "making-order:open",//переход к оформлению заказа
+  PAYMENT_METHOD = "payment:changed",//способ оплаты
+  FILLING_IN_FIELDS_WHITH_DATE = "data-field:changed",//заполнение поля данными 
+  ORDER_COMPLETION = "order-completion:complete",//заказ оформлен
+}
 
 export type productId = string;
 
@@ -11,7 +26,7 @@ export interface IProductItem {
   price: number | null
 };
 
-export type paymentType = 'online' | 'on delivery';
+type paymentType = 'online' | 'on delivery';
 
 export interface IOrderAdress {
   payment: paymentType;
@@ -49,7 +64,7 @@ export type ApiListResponse<Type> = {
     items: Type[]
 };
 
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
 type EventName = string | RegExp;
 type Subscriber = Function;
@@ -68,14 +83,11 @@ export interface ICardActions {
     onClick: (event: MouseEvent) => void;
 }
 
-export interface ICard {
-  id: string;
-  title: string;
-  price: number | null
+export interface ICard extends Pick<IProductItem, 'id' | 'title' | 'price'> {
   image?: string;
-  category?: ProductStatus;
+  category?: ProductStatus
   description?: string;
-}
+} 
 
 export interface IPage {
     counter: number;
@@ -84,6 +96,7 @@ export interface IPage {
   }
 
 export interface ISuccess {
+    id: string,
     total: number;
 }
 
@@ -93,12 +106,4 @@ export interface ISuccessActions {
 
 export interface IModalData {
     content: HTMLElement;
-}
-
-export interface ISuccess {
-    total: number;
-}
-
-export interface ISuccessActions {
-    onClick: () => void;
 }
